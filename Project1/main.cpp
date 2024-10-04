f3e783bbcc5344e3f240bf0ac5852c83c091748f
MarinaPaley <marina_paley@mail.ru>
2024-10-04 17:51:35 +0300

    locale, try

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <optional>
 #include <chrono>
+#include <locale>
 #include "../Library/Author.h"
 
 using namespace Library;
 int main()
 {
-	const std::string firstName{ "Лев" };
-	const std::string familyName{ "Толстой" };
-	const std::optional<std::string> patronymicName{ "Николаевич" };
-	const std::chrono::year_month_day birthDay{ std::chrono::September / 9 / 1828 };
-	const std::chrono::year_month_day deathDay{ std::chrono::November / 20 / 1910 };
+	setlocale(LC_ALL, "Russian");
+	try {
+		const std::string firstName{ "Лев" };
+		const std::string familyName{ "Толстой" };
+		const std::optional<std::string> patronymicName{ "Николаевич" };
+		const std::chrono::year_month_day birthDay{ std::chrono::September / 9 / 2828 };
+		const std::chrono::year_month_day deathDay{ std::chrono::November / 20 / 1910 };
 
-	Author author{ familyName , firstName, patronymicName, birthDay, deathDay };
+		Author author{ familyName , firstName, patronymicName, birthDay, deathDay };
 
-	std::cout << author.ToString();
+		std::cout << author.ToString();
+	}
+	catch (std::out_of_range& ex)
+	{
+		std::cerr << ex.what();
+	}
 
 	return 0;
 }
